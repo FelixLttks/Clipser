eeb843648d7178f747a44cdd31997ab3a9a031fd
FelixLttks <luttkusfelix@gmail.com>
2023-12-26 17:36:23 +0100

    added bootstrap

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <button type="button" className="btn btn-primary">
+      Primary
+    </button>
+  );
+};
+
+export default Button;
